2f44e9d84dd2d2ba35bf911f0e9e668902966067
yasvacouto <102669489+yasvacouto@users.noreply.github.com>
2024-08-02 08:43:08 -0300

    commit

--- script.js
+++ script.js
@@ -63,7 +63,9 @@ const perguntas = [
                           
             afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             }
+            
             {
+            
                 texto: "O chat pode ser uma tecnologia muito avançada, porém é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
                 afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
